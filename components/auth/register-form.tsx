379c6a7d8a846140a60df4fdb9b7cb5b79487371
Joel Angel <joelbaraka4325@gmail.com>
2024-09-23 16:27:47 +0300

    form function

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -1,11 +1,47 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import CardWrapper from "./card-wrapper";
 {
   /* Reusable Component that we can use for the login and register form, making variables and values dynamic*/
 }
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { RegisterSchema } from "@/schema";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useFormStatus } from "react-dom";
+
 const RegisterForm = () => {
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+  // 2. Define a submit handler.
+  const onSubmit = (data: z.infer<typeof RegisterSchema>) => {
+    setLoading(true);
+    console.log(data);
+    // Define logic to send to database or backend
+  };
+
+  const { pending } = useFormStatus();
+
   return (
     <CardWrapper
       label="Create an account"
@@ -13,7 +49,71 @@ const RegisterForm = () => {
       backButtonHref="/auth/login"
       backButtonLabel="Already have an account? Login Here"
     >
-      <div></div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="email"
+                      placeholder="iamjoelangel.com"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="John Doe" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="password" placeholder="******" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="password" placeholder="******" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" className="w-full" disabled={pending}>
+            {loading ? "Loading..." : "Register"}
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
