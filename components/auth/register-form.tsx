56612cac38aa09f5a473fbf9fdd228b6eec75bc1
Joel Angel <joel.angel@gitfitcode.com>
2024-09-23 13:22:53 +0300

    components structure

---
+++ components/auth/register-form.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const RegisterForm = () => {
+  return (
+    <div>
+      <h1>RegisterForm</h1>
+    </div>
+  );
+};
+
+export default RegisterForm;
